e14e4219c6bb646a47312fe34d86b5ba1024451b
Sushant Singh <sks1998.deactivate@gmail.com>
2020-08-22 15:11:50 +0530

    Pass down search result and render result list

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,6 +6,17 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [{name: 'a', album: 'b', artist: 'c', id: 1},
+      {name: 'd', album: 'e', artist: 'f', id: 2},
+      {name: 'g', album: 'h', artist: 'i', id: 3}]
+    }
+  }
+
+
   render() {
     return (
       <div>
@@ -13,7 +24,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
+            <SearchResults searchResults={this.state.searchResults}/>
             <Playlist />
           </div>
         </div>
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -4,9 +4,9 @@ class Track extends React.Component {
 
     renderAction() {
         if (this.props.isRemoval) {
-            return <button className="Track-action">+</button>;
+            return <button className="Track-action">-</button>;
         } else {
-            return <button className="Track-action">-</button>
+            return <button className="Track-action">+</button>
         }
     }
 
@@ -14,8 +14,8 @@ class Track extends React.Component {
         return(
             <div className="Track">
                 <div className="Track-information">
-                    <h3></h3>
-                    <p></p>
+                    <h3>{this.props.track.name}</h3>
+                    <p>{this.props.track.artist} {this.props.track.album}</p>
                 </div>
                 {this.renderAction()}
             </div>
